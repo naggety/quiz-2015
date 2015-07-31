d10325efb23f3f91c2f4d9c197b20cb5c3696d75
Iñigo Huguet <inigohuguet@hotmail.com>
2015-07-31 19:43:14 +0200

    Autorización

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,11 @@ router.post('/logout', sessionCtrllr.destroy);
 router.get('/quizes', ctrllr.list);
 router.get('/quizes/:quizId(\\d+)', ctrllr.question);
 router.get('/quizes/:quizId(\\d+)/answer', ctrllr.answer);
-router.get('/quizes/new', ctrllr.getnew);
-router.post('/quizes', ctrllr.postnew);
-router.get('/quizes/:quizId(\\d+)/edit', ctrllr.getedit);
-router.put('/quizes/:quizId(\\d+)', ctrllr.putedit);
-router.delete('/quizes/:quizId(\\d+)', ctrllr.delete);
+router.get('/quizes/new', sessionCtrllr.loginRequired, ctrllr.getnew);
+router.post('/quizes', sessionCtrllr.loginRequired, ctrllr.postnew);
+router.get('/quizes/:quizId(\\d+)/edit', sessionCtrllr.loginRequired, ctrllr.getedit);
+router.put('/quizes/:quizId(\\d+)', sessionCtrllr.loginRequired, ctrllr.putedit);
+router.delete('/quizes/:quizId(\\d+)', sessionCtrllr.loginRequired, ctrllr.delete);
 router.get('/author', ctrllr.author);
 
 router.post('/quizes/:quizId(\\d+)/comments', commentCtrllr.postnew);
